0f9a5c8c778a1e4f27e2725a9df126a42ce67cd8
yusufsalami <yusufsalami266@gmail.com>
2024-09-12 18:34:36 +0100

    note and todo

--- main.go
+++ main.go
@@ -7,10 +7,35 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
+	
 )
 
+type saver interface{
+	Save() error
+}
+
+// type displayer interface{
+// 	Display()
+// }
+
+type outputtable interface{
+	saver
+	Display()
+}
+
 func main() {
+
+	printSomething(1.5)
 	title, content:= getNoteData()
+	todoText := getUserInput("Todo text: ")
+
+	todo , err := todo.New(todoText)
+
+	if err!= nil {
+		fmt.Println(err)
+		return
+	}
 
 	userNote, err :=note.New(title, content)
 
@@ -18,15 +43,66 @@ func main() {
 		fmt.Print(err)
 		return
 	}
-	userNote.Display()
-	err = userNote.Save()
+
+	// todo.Display()
+	// err = saveData(todo)
+	err = outputData(todo)
+	if err != nil {
+		return
+	}
+	
+
+	// userNote.Display()
+	// err = saveData(userNote)
+	err = outputData(userNote)
+	if err != nil{
+		return
+	}
+}
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+func printSomething(value interface{}){
+	intVal, ok := value.(int)
+
+	if ok {
+		fmt.Println("Integer:", intVal)
+		return
+	}
+
+	floatVal, ok := value.(float64)
+
+	if ok {
+		fmt.Println("Float:", floatVal)
+		return
+	}
+	stringVal, ok := value.(string)
+
+	if ok {
+		fmt.Println("String:", stringVal)
+		return
+	}
+
+	// switch value.(type){
+	// case int:
+	// 	fmt.Println("Integer:", value)
+	// case float64:
+	// 	fmt.Println("Float:", value)
+	// case string:
+	// 	fmt.Println("String:", value)
+	// }
+}
+
+func saveData(data saver) error {
+	err := data.Save()
 
 	if err != nil {
 		fmt.Println("Saving the note failed")
-		return
+		return err
 	}
 	fmt.Println("Saving the note succeeded!")
-	
+	return nil
 }
 
 func getNoteData()(string, string){
